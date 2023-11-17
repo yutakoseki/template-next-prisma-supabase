c1e4730c5b2141fd8e755cb1e90d985f5defa739
ksk <emailofyuta@gmail.com>
2023-11-18 06:00:36 +0900

    テンプレ完成

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // 全ての API routes にマッチ
+    async headers() {
+      return [
+        {
+          // 対象APIのパスパターン
+          // 今回は src/app/api/ 配下にAPIを作っているので下記のようにする
+          source: "/api/:path*",
+          headers: [
+            {
+              // CORSを許可するオリジン
+              key: "Access-Control-Allow-Origin",
+              // すべてのオリジンを許可するなら * (アスタリスク)
+              // ただセキュリティ的にはよろしくないので注意
+              value: "https://sample-prisma-next-app.vercel.app",
+            },
+            {
+              // 許可するメソッド
+              key: "Access-Control-Allow-Methods",
+              value: "GET,OPTIONS,POST",
+            },
+            {
+              // 許可するリクエストヘッダ
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
